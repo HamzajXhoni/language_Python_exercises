77db5c2f8a43c5d7ca70c96cddaaef6183504eee
HamzajXhoni <77117966+HamzajXhoni@users.noreply.github.com>
2022-02-14 11:14:50 +0100

    Add files via upload

---
+++ Es_5.py
@@ -0,0 +1,11 @@
+while True:
+	print("Inserisci un numero ")
+	N1=int(input())
+	print("Inserisci un numero maggiore di N1")
+	N2=int(input())
+	if N1<N2:
+	    while N1!= N2+1:
+	        print(N1)
+	        N1+=1
+	else: continue
+
